4d9e0009d39c35dc644a8a9fabc85b1bb3cc3eb1
joashcabanilla <joash0199@gmail.com>
2021-01-02 10:53:28 +0800

    sign up update 1/2/2021

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="description" content="Professor Evaluation System">
-<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,maiximum-scale=1.0,minimum-scale=1.0">
+<meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0">
 <link rel="shortcut icon" href="image/icon.ico">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
